1c1e470d09de0cef4dafc9acc6cf1f66f8c35b4f
bodyisnumb <goblimgus@gmail.com>
2024-07-01 23:04:40 +0300

    game v1.0

---
+++ Assets/Scripts/Slot.cs
@@ -0,0 +1,125 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum SlotValue
+{
+    Crown,
+    Diamond,
+    Seven,
+    Cherry,
+    Bar
+}
+
+public class Slot : MonoBehaviour
+{
+    private int randomValue;
+    [HideInInspector] public float timeInterval;
+    private float speed;
+    public SlotValue stoppedSlot;
+    private SlotMachine sm;
+
+    private void Start()
+    {
+        sm = gameObject.GetComponentInParent<SlotMachine>();
+    }
+
+    public IEnumerator Spin()
+    {
+        timeInterval = sm.timeInterval;
+        randomValue = Random.Range(0, 90);
+        speed = 300f + randomValue;
+        while (speed >= 10f)
+        {
+            speed -= Time.deltaTime * 50;
+            transform.Translate(Vector2.up * Time.deltaTime * -speed);
+            if (transform.localPosition.y <= -2.1f)
+            {
+                transform.localPosition = new Vector2(transform.localPosition.x, 3.4f);
+            }
+            yield return null;
+        }
+        StartCoroutine("EndSpin");
+        yield return null;
+    }
+
+    private IEnumerator EndSpin()
+    {
+        while (speed >= 2f)
+        {
+            if (transform.localPosition.y < -0.38f)
+            {
+                transform.localPosition = Vector2.MoveTowards(transform.localPosition, new Vector2(transform.localPosition.x, -1.3f), speed * Time.deltaTime);
+                if (Mathf.Abs(transform.localPosition.y - (-1.3f)) < 0.1f)
+                {
+                    speed = 0;
+                }
+            }
+            else if (transform.localPosition.y < 0.6f)
+            {
+                transform.localPosition = Vector2.MoveTowards(transform.localPosition, new Vector2(transform.localPosition.x, -0.38f), speed * Time.deltaTime);
+                if (Mathf.Abs(transform.localPosition.y - (-0.38f)) < 0.1f)
+                {
+                    speed = 0;
+                }
+            }
+            else if (transform.localPosition.y < 1.53f)
+            {
+                transform.localPosition = Vector2.MoveTowards(transform.localPosition, new Vector2(transform.localPosition.x, 0.6f), speed * Time.deltaTime);
+                if (Mathf.Abs(transform.localPosition.y - 0.6f) < 0.1f)
+                {
+                    speed = 0;
+                }
+            }
+            else if (transform.localPosition.y < 2.5f)
+            {
+                transform.localPosition = Vector2.MoveTowards(transform.localPosition, new Vector2(transform.localPosition.x, 1.53f), speed * Time.deltaTime);
+                if (Mathf.Abs(transform.localPosition.y - 1.53f) < 0.1f)
+                {
+                    speed = 0;
+                }
+            }
+            else if (transform.localPosition.y < 3.4f)
+            {
+                transform.localPosition = Vector2.MoveTowards(transform.localPosition, new Vector2(transform.localPosition.x, 2.5f), speed * Time.deltaTime);
+                if (Mathf.Abs(transform.localPosition.y - 2.5f) < 0.1f)
+                {
+                    speed = 0;
+                }
+            }
+            speed = speed / 1.01f;
+            yield return new WaitForSeconds(timeInterval);
+        }
+        speed = 0;
+        CheckResults();
+        yield return null;
+    }
+
+    private void CheckResults()
+    {
+        float tolerance = 0.35f; // Adjust tolerance as needed
+
+        if (Mathf.Abs(transform.localPosition.y - (-1.3f)) < tolerance)
+        {
+            stoppedSlot = SlotValue.Crown;
+        }
+        else if (Mathf.Abs(transform.localPosition.y - (-0.38f)) < tolerance)
+        {
+            stoppedSlot = SlotValue.Diamond;
+        }
+        else if (Mathf.Abs(transform.localPosition.y - 0.6f) < tolerance)
+        {
+            stoppedSlot = SlotValue.Seven;
+        }
+        else if (Mathf.Abs(transform.localPosition.y - 1.53f) < tolerance)
+        {
+            stoppedSlot = SlotValue.Cherry;
+        }
+        else if (Mathf.Abs(transform.localPosition.y - 2.5f) < tolerance)
+        {
+            stoppedSlot = SlotValue.Bar;
+        }
+
+        sm.WaitResults();
+    }
+}
---
+++ Assets/Scripts/SlotMachine.cs
@@ -0,0 +1,174 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class SlotMachine : MonoBehaviour
+{
+    public TMP_Text moneyText;
+    private int price = 1;
+    public Slot[] slots;
+    public Combinations[] combinations;
+    public float timeInterval = 0.025f;
+    private int stoppedSlots = 3;
+    private bool isSpin = false;
+    public bool isAuto;
+    public Animator btnAnim;
+    public TMP_Text betAmountText;
+    private int betAmount = 1;
+    private int money;
+    private int consecutiveLosses = 0;
+    public GameObject freeSpinPanel;
+
+
+    private void Awake()
+    {
+        Application.targetFrameRate = 120;
+    }
+    private void Start()
+    {
+        btnAnim = GameObject.FindGameObjectWithTag("AutoButton").GetComponent<Animator>();
+        money = PlayerPrefs.GetInt("Money", 1000);
+        UpdateMoneyText();
+        UpdateBetAmountText();
+        freeSpinPanel.SetActive(false);
+    }
+
+    public void Spin()
+    {
+        if (!isSpin && (money - price * betAmount >= 0 || consecutiveLosses >= 3))
+        {
+            if (consecutiveLosses >= 3)
+            {
+                freeSpinPanel.SetActive(false);
+                consecutiveLosses = 0;
+            }
+            else
+            {
+                ChangeMoney(-price * betAmount);
+            }
+
+            isSpin = true;
+            foreach (Slot i in slots)
+            {
+                i.StartCoroutine("Spin");
+            }
+        }
+    }
+
+    public void WaitResults()
+    {
+        stoppedSlots -= 1;
+        if (stoppedSlots <= 0)
+        {
+            stoppedSlots = 3;
+            CheckResults();
+        }
+    }
+
+    public void CheckResults()
+    {
+        isSpin = false;
+        bool win = false;
+
+        foreach (Combinations i in combinations)
+        {
+            if (slots[0].gameObject.GetComponent<Slot>().stoppedSlot.ToString() == i.FirstValue.ToString()
+                && slots[1].gameObject.GetComponent<Slot>().stoppedSlot.ToString() == i.SecondValue.ToString()
+                && slots[2].gameObject.GetComponent<Slot>().stoppedSlot.ToString() == i.ThirdValue.ToString())
+            {
+                ChangeMoney(i.prize * betAmount);
+                win = true;
+            }
+        }
+
+        if (!win)
+        {
+            consecutiveLosses++;
+            if (consecutiveLosses >= 3)
+            {
+                freeSpinPanel.SetActive(true);
+            }
+        }
+        else
+        {
+            consecutiveLosses = 0;
+        }
+
+        if (isAuto)
+        {
+            Invoke("Spin", 0.4f);
+        }
+    }
+
+    private void ChangeMoney(int count)
+    {
+        money += count;
+        PlayerPrefs.SetInt("Money", money);
+        UpdateMoneyText();
+    }
+
+    private void UpdateMoneyText()
+    {
+        moneyText.text = "MONEY: " + money.ToString();
+    }
+
+    public void SetAuto()
+    {
+        if (!isAuto)
+        {
+            timeInterval = timeInterval / 10;
+            isAuto = true;
+            btnAnim.SetBool("isAuto", true);
+            Spin();
+        }
+        else
+        {
+            timeInterval = timeInterval * 10;
+            isAuto = false;
+            btnAnim.SetBool("isAuto", false);
+        }
+    }
+
+    public void IncreaseBetAmount()
+    {
+        if (betAmount < 5)
+        {
+            betAmount++;
+            UpdateBetAmountText();
+        }
+    }
+
+    public void DecreaseBetAmount()
+    {
+        if (betAmount > 1)
+        {
+            betAmount--;
+            UpdateBetAmountText();
+        }
+    }
+
+    private void UpdateBetAmountText()
+    {
+        betAmountText.text = "Bet: " + betAmount.ToString();
+    }
+}
+
+[System.Serializable]
+public class Combinations
+{
+    public enum SlotValue
+    {
+        Crown,
+        Diamond,
+        Seven,
+        Cherry,
+        Bar
+    }
+
+    public SlotValue FirstValue;
+    public SlotValue SecondValue;
+    public SlotValue ThirdValue;
+    public int prize;
+}
